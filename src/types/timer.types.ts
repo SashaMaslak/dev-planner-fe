247248c8887fbced9335c631b66b40e1de1c122d
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2024-08-22 15:11:23 +0300

    in progress 1 commit

---
+++ src/types/timer.types.ts
@@ -0,0 +1,15 @@
+import { IBase } from "./root.types"
+
+export interface ITimerRoundResponse extends IBase {
+  isCompleted?: boolean
+  totalSeconds: number
+}
+
+export interface ITimerSessionResponse extends IBase {
+  isCompleted?: boolean
+  rounds?: ITimerRoundResponse[]
+}
+
+export type TypeTimerSessionState = Partial<Omit<ITimerSessionResponse, 'id' | 'createdAt' | 'updatedAt'>>
+
+export type TypeTimerRoundState = Partial<Omit<ITimerRoundResponse, 'id' | 'createdAt' | 'updatedAt'>>
